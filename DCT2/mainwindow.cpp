ed59cc4c429ed5fa69b961848953d5a70196c23c
Davide Costantini <davidecostantini30@gmail.com>
2024-07-02 19:18:23 +0200

    Implemented image selection

--- DCT2/mainwindow.cpp
+++ DCT2/mainwindow.cpp
@@ -1,12 +1,45 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    lastPath = QDir("/");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_selectImageButton_clicked()
+{
+    QString filter = "Images (*.bmp)";
+    QString filePath = QFileDialog::getOpenFileName(this, "Select image", lastPath.path(), filter);
+
+    if (filePath == "")
+        return;
+
+    setImagePreview(filePath);
+    ui->filePathLineEdit->setText(filePath);
+    lastPath = getPath(filePath);
+}
+
+void MainWindow::setImagePreview(QString path)
+{
+    QPixmap pixmap(path);
+    if (pixmap.isNull())
+        return;
+
+    int imageWidth = ui->imagePreviewLabel->width();
+    int imageHeight = ui->imagePreviewLabel->height();
+    ui->imagePreviewLabel->setPixmap(pixmap.scaled(imageWidth, imageHeight, Qt::KeepAspectRatio));
+}
+
+QDir getPath(QString filePath)
+{
+    QFileInfo file(filePath);
+    return file.absoluteDir();
 }
--- DCT2/mainwindow.h
+++ DCT2/mainwindow.h
@@ -2,22 +2,29 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDir>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    private slots:
+        void on_selectImageButton_clicked();
 
-private:
-    Ui::MainWindow *ui;
+    private:
+        void setImagePreview(QString path);
+        Ui::MainWindow *ui;
+        QDir lastPath;
 };
+
+QDir getPath(QString filePath);
 #endif // MAINWINDOW_H
